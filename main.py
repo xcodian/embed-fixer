1e3c70eaa5025585ab16f64d22b84f50f761a02f
Codian <44289933+xxcodianxx@users.noreply.github.com>
2022-03-02 22:37:58 +1030

    Cleanup

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+"""
+EMBED-FIXER
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 import json
 import inspect
 
 import discord
-from discord.ext import commands
-
 bot = discord.Client()
 
 def log(s: str, level: str = 'info'):
@@ -29,18 +44,18 @@ async def on_message(msg: discord.Message):
     if 'https://media.discordapp.net' in msg.content:
         # await msg.delete()
         await msg.reply(
-            ':hammer_pick: Changed **media.discordapp.net** to **cdn.discordapp.com** so the media plays properly! You\'re welcome!\n' 
+            ':hammer_pick: Changed **media.discordapp.net** to **cdn.discordapp.com** so the media plays properly! You\'re welcome!\n'
             + msg.content.replace(
                 'https://media.discordapp.net', 'https://cdn.discordapp.com', 1
             )
         )
 
-if __name__ == '__main__':
-    def write_config(_config):
-        """Writes the config file to disk."""
-        with open('config.json', 'w+') as f:
-            json.dump(_config, f, indent=4)
-    
+def write_config(_config):
+    """Writes the config file to disk."""
+    with open('config.json', 'w+', encoding='utf-8') as f:
+        json.dump(_config, f, indent=4)
+
+def main():
     config = {
         'token': ''
     }
@@ -48,7 +63,7 @@ if __name__ == '__main__':
     try:
         with open('config.json', 'r', encoding='utf-8') as f:
             config = json.load(f)
-    except:
+    except FileNotFoundError:
         log('config file created now, token will probably be invalid', 'warn')
     finally:
         write_config(config)
@@ -60,3 +75,7 @@ if __name__ == '__main__':
         )
     except discord.LoginFailure as e:
         log(f'login failed: {e}', 'fatal')
+
+
+if __name__ == '__main__':
+    main()
